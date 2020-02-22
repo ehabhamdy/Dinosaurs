d19ae4eef2cb7bf001dc2df3e4e4681f8fc1b7af
ehabhamdy2012 <ehabhamdy2012@gmail.com>
2020-02-22 02:27:18 +0100

    Complete the implementation

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ let deno1 = new Dino(
   "herbavor",
   "North America",
   "Late Cretaceous",
-  "The largest known skull measures in at 5 feet long.",
+  [
+    "The largest known skull measures in at 5 feet long.",
+    "Triceratops Fact #2"
+  ],
   "./images/triceratops.png"
 );
 
@@ -31,7 +34,10 @@ let deno2 = new Dino(
   "carnivor",
   "North America",
   "Late Cretaceous",
-  "The largest known skull measures in at 5 feet long.",
+  [
+    "The largest known skull measures in at 5 feet long.",
+    "Tyrannosaurus Rex Fact #2"
+  ],
   "./images/tyrannosaurus rex.png"
 );
 
@@ -42,7 +48,10 @@ let deno3 = new Dino(
   "herbavor",
   "North America",
   "Late Cretaceous",
-  "Anklyosaurus survived for approximately 135 million years.",
+  [
+    "Anklyosaurus survived for approximately 135 million years.",
+    "Anklyosaurus Fact #2"
+  ],
   "./images/anklyosaurus.png"
 );
 
@@ -53,7 +62,10 @@ let deno4 = new Dino(
   "herbavor",
   "North America",
   "Late Jurasic",
-  "An asteroid was named 9954 Brachiosaurus in 1991.",
+  [
+    "An asteroid was named 9954 Brachiosaurus in 1991.",
+    "Brachiosaurus Fact #2"
+  ],
   "./images/brachiosaurus.png"
 );
 
@@ -64,7 +76,10 @@ let deno5 = new Dino(
   "herbavor",
   "North America, Europe, Asia",
   "Late Jurasic to Early Cretaceous",
-  "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+  [
+    "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+    "Stegosaurus Fact #2"
+  ],
   "./images/stegosaurus.png"
 );
 
@@ -75,7 +90,10 @@ let deno6 = new Dino(
   "carnivor",
   "North America",
   "Late Cretaceous",
-  "Elasmosaurus was a marine reptile first discovered in Kansas.",
+  [
+    "Elasmosaurus was a marine reptile first discovered in Kansas.",
+    "Elasmosaurus Fact #2"
+  ],
   "./images/elasmosaurus.png"
 );
 
@@ -86,7 +104,10 @@ let deno7 = new Dino(
   "carnivor",
   "North America",
   "Late Cretaceous",
-  "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+  [
+    "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+    "Pteranodon Fact #2"
+  ],
   "./images/pteranodon.png"
 );
 
@@ -97,32 +118,62 @@ let deno8 = new Dino(
   "herbavor",
   "World Wide",
   "Holocene",
-  "All birds are living dinosaurs.",
+  ["All birds are living dinosaurs.", "Pigeon Fact #2"],
   "./images/pigeon.png"
 );
-
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
 Dino.prototype.compareName = function(dinoToCompare) {
-    console.log(this.fact);
-  };
+  return `${this.species} vs ${dinoToCompare.species}`;
+};
 
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
 Dino.prototype.compareHight = function(dinoToCompare) {
-    console.log(this.fact);
-  };
+  if (this.height > dinoToCompare.height) {
+    return "Dino is taller";
+  } else {
+    return "Human is taller";
+  }
+};
 
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
 Dino.prototype.compareDiet = function(dinoToCompare) {
-    console.log(this.fact);
-  };
+  return `${this.diet} vs ${dinoToCompare.diet}`;
+};
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  for (let i in array) {
+    if (array[i].where == "Human") {
+      let tmp = array[4];
+      array[4] = array[i];
+      array[i] = tmp;
+    }
+  }
+
+  return array;
+}
 
 const grid = document.getElementById("grid");
 grid.style.display = "none";
-
 
 // Remove form from screen
 // On button click, prepare and display infographic
@@ -130,35 +181,30 @@ const compareButton = document.getElementById("btn");
 const form = document.getElementById("dino-compare");
 
 compareButton.addEventListener("click", function() {
-  console.log("clicked");
   form.style.display = "none";
   grid.style.display = "flex";
 
-
   // Use IIFE to get human data from form
-let humanFromForm = (function getHumanData() {
-    console.log("IIFEE");
+  let humanFromForm = (function getHumanData() {
     const nameInput = document.getElementById("name").value;
-    const feetsHeightInput = document.getElementById("feet").value;
     const inchesHeightInput = document.getElementById("inches").value;
     const weightInput = document.getElementById("weight").value;
     const dietInput = document.getElementById("diet").value;
-  
-    console.log(nameInput);
+
     // Create Human Object
     let human = new Dino(
       nameInput,
       weightInput,
       inchesHeightInput,
       dietInput,
-      "World Wide",
+      "Human",
       "Late Cretaceous",
-      "",
+      [""],
       "./images/human.png"
     );
     return human;
   })();
-  
+
   let denosArray = [
     deno1,
     deno2,
@@ -171,16 +217,20 @@ let humanFromForm = (function getHumanData() {
     deno8
   ];
 
+  const randomizedDenosArray = shuffle(denosArray);
+
   grid.innerHTML = (() => {
     return `
-          ${denosArray
+          ${randomizedDenosArray
             .map(
               deno => `
               <div class="grid-item">
                 <h3>${deno.species}</h3>
                 <img src="${deno.img}" />
                 <p> 
-                    ${deno.fact} 
+                    ${deno.fact[Math.floor(Math.random() * deno.fact.length)]}
+                    <br>
+                    ${deno.compareName(humanFromForm)}
                 </p>
               </div>
           `
